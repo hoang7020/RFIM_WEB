afa14ee3746034bce0f4e51f4a0c6f7b697c1f68
hieu do <hieu.dotrung96@gmail.com>
2019-07-07 20:35:52 -0700

    no mess

--- RFIM_Web/obj/Debug/netcoreapp2.1/Razor/Views/Invoice/DeleteInvoice.g.cshtml.cs
+++ RFIM_Web/obj/Debug/netcoreapp2.1/Razor/Views/Invoice/DeleteInvoice.g.cshtml.cs
@@ -61,12 +61,12 @@ using RFIM_Web.Models;
             WriteLiteral("\r\n");
             EndContext();
             BeginContext(34, 505, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("form", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "0f2ff5d1edb74607bf16c31fdb085847", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("form", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "673f74ba200d44fa933ba775e3890ff3", async() => {
                 BeginContext(81, 42, true);
                 WriteLiteral("\r\n    <div class=\"modal-header\">\r\n        ");
                 EndContext();
                 BeginContext(123, 109, false);
-                __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "e5b1abc20cf64ad59bb24c43f73752c4", async() => {
+                __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "9dcda9643c9f46838030c39817296887", async() => {
                     BeginContext(221, 7, true);
                     WriteLiteral("&times;");
                     EndContext();
