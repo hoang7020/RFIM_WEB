293d78dc175973e2917f052351ee5de72912691a
godverkan <Kirovasra@gmail.com>
2019-07-17 23:28:50 +0700

    Invoice xịn xò

---
+++ RFIM_Web/ModelView/ProductDetailList.cs
@@ -0,0 +1,15 @@
+﻿using RFIM_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RFIM_Web.ModelView
+{
+    public class ProductDetailList
+    {
+        public Product Product { get; set; }
+        public Category Category { get; set; }
+        public Vendor Vendor { get; set; }
+    }
+}
